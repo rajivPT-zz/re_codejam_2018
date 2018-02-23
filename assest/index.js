2b455959416e5388a5b6233f2ada9370ad77fb32
Rajiv <rajivsomani@PT-IT-000677.local>
2018-02-23 11:39:46 +0530

    minor changes

--- assest/index.js
+++ assest/index.js
@@ -1,7 +1,7 @@
 var web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 var abi = JSON.parse('[{"constant":true,"inputs":[],"name":"totalConfirmationRequired","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalAmount","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"brokerAcc","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"payToBroker","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"resetTotalConfirmation","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"getBuilderAccount","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"confirmPayment","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"delay","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"buyerAcc","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getBuyerAccount","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"payToBuilder","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"builderAcc","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"payToBuyer","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"totalConfirmation","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getDelay","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"getBrokerAccount","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"brokerComm","outputs":[{"name":"","type":"uint128"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getTotalRequiredConfirmation","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"getTotalConfirmation","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"buyerComm","outputs":[{"name":"","type":"uint128"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"totalAmounti","type":"int256"},{"name":"brokerCommi","type":"uint128"},{"name":"buyerCommi","type":"uint128"},{"name":"brokerAcci","type":"address"},{"name":"builderAcci","type":"address"},{"name":"buyerAcci","type":"address"},{"name":"delayi","type":"int256"},{"name":"totalConfirmationRequiredi","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
 var VotingContract = web3.eth.contract(abi);
-var contractNum = '0x12462fe41a1a3fd78c3432f11332d0acb23ad27e';
+var contractNum = '0xc7706f2a3976c12b2331fd48eeb958679e1704c0';
 var contractInstance = VotingContract.at(contractNum);
 var timer;
 var autoTransfer;
@@ -11,11 +11,12 @@ var brokerBal = null;
 
 function payToBuilder() {
   clearInterval(timer);
+  console.log(contractInstance.getTotalConfirmation.call()>=contractInstance.getTotalRequiredConfirmation.call());
   if(contractInstance.getTotalConfirmation.call()>=contractInstance.getTotalRequiredConfirmation.call()){
   web3.eth.sendTransaction({from: web3.eth.coinbase, to: contractInstance.getBrokerAccount.call(), value: web3.toWei(contractInstance.payToBroker.call(), "ether")})
   web3.eth.sendTransaction({from: web3.eth.coinbase, to: contractInstance.getBuilderAccount.call(), value: web3.toWei(contractInstance.payToBuilder.call(), "ether")}) 
   contractInstance.resetTotalConfirmation({from:web3.eth.coinbase});
-  $(".action").removeClass('done');
+  $(".action").removeClass('done check');
   verifiedCount = 0;
   timer = setInterval(function(){
     if(brokerBal && brokerBal !== web3.fromWei(web3.eth.getBalance(contractInstance.getBrokerAccount.call()), 'ether').c[0]){
@@ -130,7 +131,7 @@ function btnActionHandler(that){
     return false
   }
   $(".action").removeClass('animate');
-  $(that).addClass('done');
+  $(that).addClass('done check');
   $($(".action").get($(".action").index(that)+1)).addClass('animate');
   ($(that).data('type') !== 'bank') ? (contractInstance.confirmPayment({from:web3.eth.coinbase}), checkTransaction(), verifiedCount++) : payToBuilder();
 }
@@ -155,6 +156,6 @@ $(document).ready(function() {
   setTimeout(function(){
     lastBlock = web3.eth.blockNumber;
     console.log(web3.eth.blockNumber > 1000 ? web3.eth.blockNumber-100 : web3.eth.blockNumber);
-    getAccountTransactions(contractNum, (web3.eth.blockNumber > 1000 ? web3.eth.blockNumber-100 : web3.eth.blockNumber) , lastBlock);
+    getAccountTransactions(contractNum, 0 , lastBlock);
   },1000);
 });
